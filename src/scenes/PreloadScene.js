a8547b164f175dad2fecc2217f972b06a82989fc
Xtremind <guillaume.dagraca@gmail.com>
2018-06-25 08:47:19 +0000

    add player with move

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -28,18 +28,21 @@ class PreloadScene extends Phaser.Scene {
 
     this.load.image('background', 'public/items/bg_castle.png');
 
+    // tiles in spritesheet 
     this.load.image('tiles', 'public/items/tileset.png');
-    this.load.tilemapTiledJSON('map', 'public/items/teest.json');
+    //this.load.spritesheet('tiles', 'assets/tiles.png', {frameWidth: 70, frameHeight: 70});
+    this.load.tilemapTiledJSON('map', 'public/items/tileset.json');
 
-    this.load.image('player1', 'public/items/player1.png', 'public/items/player1.json');
-    this.load.image('player2', 'public/items/player2.png', 'public/items/player2.json');
-    this.load.image('player3', 'public/items/player3.png', 'public/items/player3.json');
+    this.load.image('player1', 'public/preview/Player/p1_front.png');
+    this.load.atlas('player2', 'public/items/player2.png', 'public/items/player2.json');
+    this.load.atlas('player3', 'public/items/player3.png', 'public/items/player3.json');
 
     // demo
     //this.load.setBaseURL(' http://labs.phaser.io');
+    /*
     this.load.image('sky', 'http://labs.phaser.io/assets/skies/space3.png');
     this.load.image('logo', 'http://labs.phaser.io/assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'http://labs.phaser.io/assets/particles/red.png');
+    this.load.image('red', 'http://labs.phaser.io/assets/particles/red.png');*/
   }
 
 }
--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,5 +1,6 @@
 
 class TitleScene extends Phaser.Scene {
+
   constructor(test) {
     super({
       key: 'TitleScene'
@@ -14,30 +15,64 @@ class TitleScene extends Phaser.Scene {
 
     //add repeating background image
     //var background = this.add.tileSprite(0, 0, this.game.width, this.game.height, "background");
-    var background = this.add.tileSprite(0, 0, 1050*2, 700*2, "background"); //don't know why, but it didn't repeat until the end of the canvas
+    var background = this.add.tileSprite(0, 0, 1050 * 2, 700 * 2, "background"); //don't know why, but it didn't repeat until the end of the canvas
 
     //add level tileset
     var map = this.make.tilemap({ key: 'map' });
     var tiles = map.addTilesetImage('cybernoid', 'tiles');
     var layer = map.createStaticLayer(0, tiles, 0, 0);
+    //var layer = map.createDynamicLayer('World', tiles, 0, 0);
+    layer.setCollisionByExclusion([-1, 0]);
+
+    // set the boundaries of our game world
+    this.physics.world.bounds.width = layer.width;
+    this.physics.world.bounds.height = layer.height;
+
+    // create the player sprite    
+    this.player = this.physics.add.sprite(200, 200, 'player1');
+    this.player.setBounce(0.2); // our player will bounce from items
+    this.player.setCollideWorldBounds(true); // don't go out of the map
+
+    // set collision
+    this.physics.add.collider(layer, this.player);
+
+
+
 
     //demo
     //this.add.image(400, 300, 'sky');
+    /*
+        var particles = this.add.particles('red');
+    
+        var emitter = particles.createEmitter({
+          speed: 100,
+          scale: { start: 1, end: 0 },
+          blendMode: 'ADD'
+        });
+        var logo = this.physics.add.image(400, 100, 'logo');
+    
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+    
+        emitter.startFollow(logo);*/
+  }
 
-    var particles = this.add.particles('red');
 
-    var emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: 'ADD'
-    });
-    var logo = this.physics.add.image(400, 100, 'logo');
+  update() {
+    // Move player
 
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    var cursors = this.input.keyboard.createCursorKeys();
 
-    emitter.startFollow(logo);
+    if (cursors.left.isDown) {
+      this.player.body.setVelocityX(-200); // move left
+    } else if (cursors.right.isDown) {
+      this.player.body.setVelocityX(200); // move right
+    }
+
+    if ((cursors.space.isDown || cursors.up.isDown) && this.player.body.onFloor()) {
+      this.player.body.setVelocityY(-500); // jump up
+    }
   }
 }
 
