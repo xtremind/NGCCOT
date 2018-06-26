3de9c4ea12fd51f985f4a6e5d6394db5f925271f
Xtremind <guillaume.dagraca@gmail.com>
2018-06-26 18:06:23 +0000

    add animations for player2

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -33,12 +33,29 @@ class TitleScene extends Phaser.Scene {
     this.player1.setBounce(0.2); // our player will bounce from items
     this.player1.setCollideWorldBounds(true); // don't go out of the map
 
-  //Load the initial sprite
-  this.player2 = this.physics.add.sprite(400, 180, 'player2', 'p2_front.png');
-  this.player2.setBounce(0.2); // our player will bounce from items
-  this.player2.setCollideWorldBounds(true); // don't go out of the map
+    //Load the initial sprite
+    this.player2 = this.physics.add.sprite(400, 180, 'player2', 'p2_front.png');
+    this.player2.setBounce(0.2); // our player will bounce from items
+    this.player2.setCollideWorldBounds(true); // don't go out of the map
 
+    this.anims.create({
+      key: 'p2_idle',
+      frames: [{ key: 'player2', frame: 'p2_front.png' }],
+      frameRate: 10,
+    });
 
+    this.anims.create({
+      key: 'p2_jump',
+      frames: [{ key: 'player2', frame: 'p2_jump.png' }],
+      frameRate: 10,
+    });
+
+    this.anims.create({
+      key: 'p2_walk',
+      frames: this.anims.generateFrameNames('player2', { prefix: 'p2_walk', suffix: '.png', start: 1, end: 11, zeroPad: 2 }),
+      frameRate: 10,
+      repeat: -1
+    });
 
     // set collision
     this.physics.add.collider(layer, this.player1);
@@ -79,18 +96,26 @@ class TitleScene extends Phaser.Scene {
       this.player1.body.setVelocityX(200); // move right
     }
 
-    if ( cursors.up.isDown && this.player1.body.onFloor()) {
+    if (cursors.up.isDown && this.player1.body.onFloor()) {
       this.player1.body.setVelocityY(-500); // jump up
     }
 
     if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q).isDown) {
       this.player2.body.setVelocityX(-200); // move left
+      this.player2.anims.play('p2_walk', true); // play walk animation
+      this.player2.flipX = true; // flip the sprite to the left
     } else if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D).isDown) {
       this.player2.body.setVelocityX(200); // move right
+      this.player2.anims.play('p2_walk', true); // play walk animation
+      this.player2.flipX = false; // flip the sprite to the right
+    } else {
+      this.player2.body.setVelocityX(0);
+      this.player2.anims.play('p2_idle', true);
     }
 
-    if ( this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z).isDown && this.player2.body.onFloor()) {
+    if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z).isDown && this.player2.body.onFloor()) {
       this.player2.body.setVelocityY(-500); // jump up
+      this.player2.anims.play('p2_jump', true);
     }
   }
 }
